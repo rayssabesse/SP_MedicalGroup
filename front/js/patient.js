663b97bcca318f437787dbdcaa662dc4346990eb
new name <new email>
2021-12-13 11:00:28 -0300

    fourth

--- front/js/patient.js
+++ front/js/patient.js
@@ -1,12 +1,1 @@
 // FLIP CARD
-function flip(event){
-	var element = event.currentTarget;
-	if (element.className === "my_profile_card") {
-    if(element.style.transform == "rotateY(180deg)") {
-      element.style.transform = "rotateY(0deg)";
-    }
-    else {
-      element.style.transform = "rotateY(180deg)";
-    }
-  }
-};
